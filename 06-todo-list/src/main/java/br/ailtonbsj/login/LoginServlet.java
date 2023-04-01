d817d4a8809a76dd5bd2610f99bc50eb256edbc5
ailtonbsj <ailton.ifce@gmail.com>
2023-04-01 15:03:59 -0300

    Reactored packages and send arrays to JSP

---
+++ 06-todo-list/src/main/java/br/ailtonbsj/login/LoginServlet.java
@@ -0,0 +1,39 @@
+package br.ailtonbsj.login;
+
+import java.io.IOException;
+
+import br.ailtonbsj.todo.TodoService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+@WebServlet(urlPatterns = "/login.do")
+public class LoginServlet extends HttpServlet {
+	LoginService loginService = new LoginService();
+	TodoService todoService = new TodoService();
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.getRequestDispatcher("WEB-INF/views/login.jsp").forward(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String login = req.getParameter("login");
+		String pass = req.getParameter("password");	
+		boolean isValid = loginService.isUserValid(login, pass);
+		
+		if(isValid) {
+			req.setAttribute("login", login);
+			req.setAttribute("todos", todoService.retrieveTodos());
+			req.getRequestDispatcher("WEB-INF/views/todo.jsp").forward(req, resp);
+		} else {
+			req.setAttribute("errorMsg", "Invalid user or password!");
+			req.getRequestDispatcher("WEB-INF/views/login.jsp").forward(req, resp);
+		}
+	}
+	
+}
